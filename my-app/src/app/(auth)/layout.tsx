7a5f82eb0a5078216d7634c35fea3c66e8e04812
rogofan <sacokov@gmail.com>
2025-06-11 00:42:16 +0200

    routingPrep

---
+++ my-app/src/app/(auth)/layout.tsx
@@ -0,0 +1,18 @@
+// export default function AuthLayout({
+//   children,
+// }: {
+//   children: React.ReactNode;
+// }) {
+//   return (
+//     <html lang="cs">
+//       <body>
+//         <div className="p-4">
+//           <a href="/" className="text-2xl font-bold">
+//             Logo
+//           </a>
+//         </div>
+//         <main>{children}</main>
+//       </body>
+//     </html>
+//   );
+// }
